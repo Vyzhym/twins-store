20174f78bcbcf559e75c923995fe9239b536d345
Artemchuk Dmytro <taruuuch@gmail.com>
2019-04-02 13:00:44 +0300

    Add dropdown menu, openstreetmap & cart popup

--- src/js/app.js
+++ src/js/app.js
@@ -14,4 +14,12 @@ $(function () {
 		autoplay: true,
 		autoplaySpeed: 5000,
 	});
+
+	var latlng = [50.44374, 30.51983],
+    	twinsMap = L.map('twinsMap').setView(latlng, 17);
+
+    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+	}).addTo(twinsMap);
+	L.marker(latlng).addTo(twinsMap);
 });
