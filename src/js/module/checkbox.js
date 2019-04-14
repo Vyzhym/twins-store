927924f6fe4bd7f12a0c6424030a7b237719ed00
Dmytro Artemchuk <taruuuch@gmail.com>
2019-04-14 22:24:07 +0300

    Clear branch

--- src/js/module/checkbox.js
+++
@@ -1,8 +0,0 @@
-(function() {
-	$('.checkbox').click(function (e) {
-		if ($(this).attr('checked') == undefined)
-			$(this).attr('checked', true);
-		else
-			$(this).attr('checked', false);
-	});
-})();
